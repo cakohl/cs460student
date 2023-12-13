6f7aedbcf900b0bc56fe7165b3265a6d381f5d16
c <c@cs-macbook-pro.ssw.umb.edu>
2023-12-13 10:16:34 -0500

    sfk1990s11

--- Project/script.js
+++ Project/script.js
@@ -87,18 +87,41 @@ const positions = [
 
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
+// ... (previous code)
+
+// Load background and character images
+const backgroundImage = new Image();
+const characterImage = new Image();
+
+backgroundImage.src = 'background.png'; 
+characterImage.src = 'player.png'; 
+
+let characterX = 100;
+let characterY = 100;
+
 // Main render function
 function drawScene() {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Render background
+  drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+  // Render character
+  drawImage(characterImage, characterX, characterY, 50, 50);
+
+  requestAnimationFrame(drawScene);
+}
+
+// Function to draw an image on the canvas
+function drawImage(image, x, y, width, height) {
+  gl.useProgram(programInfo.program);
 
   const projectionMatrix = mat4.create();
   mat4.ortho(projectionMatrix, 0, canvas.width, canvas.height, 0, -1, 1);
 
   const modelViewMatrix = mat4.create();
-  mat4.translate(modelViewMatrix, modelViewMatrix, [100, 100, 0]);
-
-  gl.useProgram(programInfo.program);
+  mat4.translate(modelViewMatrix, modelViewMatrix, [x, y, 0]);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, 2, gl.FLOAT, false, 0, 0);
@@ -107,7 +130,37 @@ function drawScene() {
   gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
   gl.uniform4fv(programInfo.uniformLocations.color, [1.0, 1.0, 1.0, 1.0]);  // White color
 
+  // Bind texture
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+  // Draw the image
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, positions.length / 2);
+}
+
+// Handle player input for character movement
+document.addEventListener('keydown', handleKeyPress);
+
+function handleKeyPress(event) {
+  const speed = 5;
+
+  switch (event.key) {
+    case 'ArrowUp':
+      characterY -= speed;
+      break;
+    case 'ArrowDown':
+      characterY += speed;
+      break;
+    case 'ArrowLeft':
+      characterX -= speed;
+      break;
+    case 'ArrowRight':
+      characterX += speed;
+      break;
+  }
 }
 
 // Initialize matrices
